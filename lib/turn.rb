1629a9a38f962a2ff1c686e7922a514d44348ff8
JC <bonassin@gmail.com>
2020-12-08 22:03:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,4 +16,17 @@ def move(board, index, character = "X")
   board[index] = character
   return board
 end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+def valid_move?(board,index)
+  if index.between?(0,8) && !position_taken?(board,index)
+    return true
+  end
+end
   
